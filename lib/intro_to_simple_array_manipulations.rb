bc2616c9e466bf7da31e0024ebdefd6a6591ac76
Sarah Drake <jussaradrake@outlook.com>
2019-07-08 20:06:13 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,58 @@
+def using_push(array, string)
+  ["Usher", "snoop dog",]
+  .push ("violet")
+end
+
+def using_unshift(array, string)
+  ["Staten Island", "snoop dog"]
+  .unshift
+end
+
+def using_pop(array)
+  ["usher", "Antarctica"]
+  .pop
+end
+
+def pop_with_args(array)
+  ["Chihuahua", "Shiba Inu"]
+  .pop(2)
+end
+
+def using_shift(array)
+  ["Lagos", "love"]
+  .shift
+end
+
+def shift_with_args(array)
+  ["Blue Bell Creameries", "Ben & Jerry's"]
+  .shift(2)
+end
+
+def using_concat(array,element)
+a = ["raindrops on roses", "whiskers on kittens", "sports cars", "flatiron school"]
+.concat
+end
+
+def using_insert(array, string)
+  ["violet", "flower","lilly", "lilly", "Python"]
+  .insert(4)
+end
+
+def using_uniq(array)
+  ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler"]
+  .uniq
+end
+
+def using_flatten(array)
+  ["Saxophone", "Piano", "Trumpet", "Violin", "Drums", "Flute"]
+  .flatten
+end
+
+def using_delete(instructors, steven)
+  instructors.delete(steven)
+end
+
+def using_delete_at(array, string)
+  ["Josh", "Sophie", "Robocop"]
+  .delete_at(2)
+end
